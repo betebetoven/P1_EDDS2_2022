70c1b2902e8814a9e374151f92c8e3070a319148
betebetoven <67440333+betebetoven@users.noreply.github.com>
2022-08-19 23:04:09 -0600

    'asdf'

---
+++ problema3/problema3.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include <math.h>
+
+
+using namespace std;
+
+int recursiva (int n) {
+if (n <= 1)
+return 1 ;
+else
+return recursiva (n/2) + recursiva (n/2) ;
+}
+void norecursiva(double x)
+{
+    int cont = 0;
+    for (double i = 0; i < x/sqrt(x); i++)
+    {
+        for (double n = 0; n < pow(2.0, i); n++)
+        {
+            cont++;
+            cout<< pow(2.0, i);
+            
+            
+            
+
+        }
+        cout<<endl;
+        
+        /* code */
+    }
+    
+
+}
+int main()
+{
+    for (int i = 1; i < 21; i++)
+    {
+        int p = recursiva(i);
+        cout<<i<<": "<<p<<endl;
+    }
+    norecursiva(21);
+    
+
+
+
+    
+
+    
+    
+    
+    
+
+
+
+   
+} 
